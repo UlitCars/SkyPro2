f92492c323912b03741fd1005302d9a633c8f423
UlitCars <zashibisf@gmail.com>
2025-02-26 22:19:59 +0300

    Lesson 1.4

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,43 +1,24 @@
 package org.skypro.skyshop;
 
+import org.skypro.skyshop.product.DiscountedProduct;
+import org.skypro.skyshop.product.FixPriceProduct;
 import org.skypro.skyshop.product.Product;
 import org.skypro.skyshop.basket.ProductBasket;
+import org.skypro.skyshop.product.SimpleProduct;
 
 public class App {
     public static void main(String[] args) {
-        Product bread = new Product();
-        bread.setName("Хлеб");
-        bread.setPrice(50);
-        Product milk = new Product();
-        Product bananas = new Product();
-        Product cheese = new Product();
-        Product waffles = new Product();
-        Product fish = new Product();
-        bananas.setName("Бананы");
-        cheese.setName("Сыр");
-        waffles.setName("Вафли");
-        fish.setName("Рыба");
-        milk.setName("Молоко");
-        milk.setPrice(120);
-        cheese.setPrice(320);
-        waffles.setPrice(110);
-        fish.setPrice(340);
-        bananas.setPrice(180);
         ProductBasket test = new ProductBasket();
-        test.addProduct(bread);
-        test.addProduct(bananas);
-        test.addProduct(milk);
-        test.addProduct(cheese);
-        test.addProduct(waffles);
-        test.addProduct(fish);
+        Product toaster = new FixPriceProduct("Тостер");
+        Product book = new DiscountedProduct("Книга", 500, 10);
+        Product phone = new DiscountedProduct("Телефон", 5000, 15);
+        Product table = new FixPriceProduct("Стол");
+        Product pen = new FixPriceProduct("Ручка");
+        test.addProduct(toaster);
+        test.addProduct(book);
+        test.addProduct(phone);
+        test.addProduct(table);
+        test.addProduct(pen);
         test.printBasket();
-        System.out.println(test.basketSum());
-        System.out.println(test.availability("Рыба"));
-        System.out.println(test.availability("Молоко"));
-        test.clean();
-        test.printBasket();
-        System.out.println(test.basketSum());
-        System.out.println(test.availability("Молоко"));
-
     }
 }
